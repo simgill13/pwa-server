d3382a3dc2656e6696204f62cfb7cdc4ee853187
Sim Gill <simgill89@gmail.com>
2017-12-03 19:48:22 -0800

    updates

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const nedb    = require("nedb");
+const rest    = require("express-nedb-rest");
+const cors    = require("cors");
 const app = express();
 app.use(bodyParser.json());
 const path = require('path');
@@ -12,67 +15,39 @@ const {PORT, DATABASE_URL} = require('./config');
 
 
 
-// const{DATABASE_URL,CLIENT_ID} = require('./secret')
-// if(DATABASE_URL === null ){
-//   DATABASE_URL = process.env.PORT
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+const datastore = new nedb({ 
+  filename: "mycoffeapp.db",
+  autoload: true
 });
 
+const restAPI = rest();
+restAPI.addDatastore('coffees', datastore);
+
+app.use(cors());
+app.use('/', restAPI);
 
 
-app.post('/textmsg', (req, res) => {
 
-  // email 
 
-  
- console.log('=================HIT==============')
 
- console.log(req.body.text)
- var string = req.body.text
- var dontlookhere = 'AC4334eb5e7984bfb826c66ce782578316'; 
- var randomnumber = 'fa7a4a8ab3761c09554af5fa8ac1e5e5'; 
- 
- //require the Twilio module and create a REST client 
- var client = require('twilio')(dontlookhere, randomnumber); 
- 
-  client.messages.create({ 
-      to: "+14085057277", 
-      from: "+18316099667", 
-      body: string , 
-  }, function(err, message) { 
-      console.log(message.sid); 
-  }); 
-    res.status(200).json({msg: 'this is working'})
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
